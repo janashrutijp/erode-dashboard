3a11aea987998bac9d7002409ed476e38976be6c
Janashruti <janashruti333@gmail.com>
2025-09-02 01:54:22 +0530

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,11 @@
+# api details.py
+import os
+
+SHOPIFY_API_KEY = " "
+SHOPIFY_PASSWORD = " "
+SHOPIFY_STORE = " "
+
+EMAIL_ADDRESS = " "  # for sending report
+EMAIL_PASSWORD = " "
+
+MANAGER_EMAIL = " "  # for receiving report
